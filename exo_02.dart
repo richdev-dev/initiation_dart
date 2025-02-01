df1c5fbf496ca0b6493772c0022ea061f3fb06b9
unknown <elishamedenoukoume@gmail.com>
2025-02-01 11:51:06 -1100

    Voici le code pour l'excercice Folie Des Dés de DartDay01(version modifiée

--- exo_02.dart
+++ exo_02.dart
@@ -3,6 +3,10 @@ void main() {
   int result = lancerDe();
   print('Le résultat du lancer est :$result');
   lancerDeMultiple(4);
+  lancerDeAvecConditions(5); 
+  lancerDeEnBoucle(3); 
+}
+
   }
 
 
@@ -20,6 +24,33 @@ int lancerDe() {
      for (i=0; i<nombreDeLancers; i++){
        resultat = random.nextInt(6)+1;
     print('Le resutat des $nombreDeLancers lancer est : $resultat');
+
+
+    void lancerDeAvecConditions(int nombreDeLancers) {
+  for (int i = 0; i < nombreDeLancers; i++) {
+    int resultat = lancerDe();
+    while (resultat < 3) {
+      resultat = lancerDe(); 
+    }
+    print('Lancer ${i + 1} : $resultat');
+  }
+}
+
+void lancerDeEnBoucle(int nombreDeLancers) {
+  int compteur = 0;
+  int lancersEffectues = 0;
+
+  while (compteur < nombreDeLancers) {
+    int resultat = lancerDe();
+    lancersEffectues++;
+    if (resultat == 6) {
+      compteur++;
+    }
+    print('Lancer $lancersEffectues : $resultat');
+  }
+  print('Nombre total de lancers effectués pour atteindre $nombreDeLancers résultats de 6 : $lancersEffectues');
+}
+
       
   }
   }
